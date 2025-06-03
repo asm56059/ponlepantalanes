ac5aebfa611d1ec1dbe7416843397b83cff9d0c4
nilodude <alphone@protonmail.com>
2025-06-03 20:25:43 +0200

    show buttons onclick

--- app.js
+++ app.js
@@ -1,26 +1,27 @@
 
+
+let paloStatus = [{'letter':'','num':0 }]
+
 document.addEventListener('click', (event)=>{
-
-    if (event.target.tagName == "path"){
-        console.log(event.target.className.baseVal)
-        console.log(event.target.getAttribute("d"))
+    let rawPalo = event.target
+    if (rawPalo.tagName == "path"){
+        // TODO: el array se rellena al cargar el programa, NO al hacer click
+        // let letter=rawPalo.className.baseVal.replace('cls-','')        
+        // const palo = {
+        //     letter: letter,
+        //     num: rawPalo.getAttribute("d")
+        // }
+        // if(!paloStatus.includes(palo)){
+        //     paloStatus.push(palo)
+        // }
+        // console.table(paloStatus) 
         
-         
+        const botones = document.getElementById('botones')
+        if(botones){
+            botones.classList.add('seve')
+        }
+        // TODO: gestionar como ocultar botone
+
     }
-    
-    // if(sonVerdaderos realGs?){
-    //     abre un popup
-    // }sno{
-        // hgaz otra cosa
-    // }
-    
+
 })
-
-
-function areThey(id){
-    
-    // if(id is blabla)
-    // return true
-
-    // return false
-}
